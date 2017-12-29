90077d8cf19b039de0d12f0f904883cc1ea46ca9
Nicolai Rosenberg <nr@speedperform.com>
2017-12-29 22:14:55 +0100

    Added

---
+++ AlphaBug/Assets/MaterialUI/Scripts/BugScripts/DoRoll.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class DoRoll : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.LogError("Hi");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void TestRoll()
+    {
+        Debug.LogError("Testclick");
+    }
+
+
+}
